1476ce4d49569794a04b61dc9fc3edd8e091edd0
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-18 21:50:24 +0530

    cpp-practice-setup

---
+++ driver_functions/driver_1.cpp
@@ -0,0 +1,23 @@
+#// funtion to print sum of two numbers
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> t;
+
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        //  Solution ob;
+        //  cout<< ob.firstRepeated(arr,n)<<endl;
+    }
+    return 0;
+}
