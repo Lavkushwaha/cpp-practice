e138992325ca3f9daaef4261e5b315353a794f34
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-18 21:50:34 +0530

    cpp practice setup

---
+++ test.cpp
@@ -0,0 +1,26 @@
+// funtion to print sum of two numbers
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void sum(int a, int b)
+{
+    int c = a + b;
+    cout << "Sum of " << a << " and " << b << " is " << c << endl;
+}
+
+int main()
+{
+    sum(10, 20);
+    sum(20, 30);
+return 0;
+}
+
+
+
+
+
+
+
+
