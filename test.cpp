900b4438c9aa29b9ad28b30deb35eb8424434a02
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-19 13:46:09 +0530

    minor

--- test.cpp
+++ test.cpp
@@ -4,18 +4,84 @@
 
 using namespace std;
 
-void sum(int a, int b)
+
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+string GetRuler(const string& kingdom) {
+  // TODO: implement this method to determine the ruler name, given the kingdom.
+  string ruler = "";
+
+char chr = kingdom[ kingdom.size() - 1]; 
+if(chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' || chr == 'u')
 {
-    int c = a + b;
-    cout << "Sum of " << a << " and " << b << " is " << c << endl;
+    ruler = "Alice";
+}
+else if(chr == 'y')
+{
+    ruler = "nobody";
+}
+else{
+    ruler = "Bob";
+
+}
+  
+
+  
+
+
+
+  return ruler;
 }
 
-int main()
-{
-    sum(10, 20);
-    sum(20, 30);
-return 0;
+// bool isVovel(char k){
+//     if(k == "a" || k == "e" || k == "i" || k == "o" || k == "u"){
+//         return true;
+//     }
+// return false;
+
+// }
+
+// bool isY( char k){
+
+//     if(k == "y"){
+//         return true;
+//     }
+
+//     return false;
+
+// }
+
+int main() {
+  int testcases;
+  cin >> testcases;
+  string kingdom;
+
+  for (int t = 1; t <= testcases; ++t) {
+    cin >> kingdom;
+    cout << "Case #" << t << ": " << kingdom << " is ruled by "
+         << GetRuler(kingdom) << ".\n";
+  }
+  return 0;
 }
+
+
+
+
+// void sum(int a, int b)
+// {
+//     int c = a + b;
+//     cout << "Sum of " << a << " and " << b << " is " << c << endl;
+// }
+
+// int main()
+// {
+//     sum(10, 20);
+//     sum(20, 30);
+// return 0;
+// }
 
 
 
